01c1dded941cef08f0158b1a9b0b9bd9cf25aada
Carlos Catala <119781636+cataladev@users.noreply.github.com>
2024-07-24 19:54:21 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+### Description
+
+I found this template somewhere on Github and tweaked it a bunch, honestly dont remember where.
+
+### [live preview](https://catala.dev)
+
+Took my web-icon from here:
+https://cdn12.picryl.com/photo/2016/12/31/cat-png-feline-animals-db34cf-1024.png
+
+### Features
+
+- Fully Responsive
+- Multi-Page Layout
+- Contact Form With EmailJs
+- React-Bootstrap
+
+### Setup
+
+<pre>git clone https://github.com/cataladev/cataladotdev-portfolio-open.git</pre>
+ 
+Install required dependencies
+
+<pre>yarn install</pre>
+
+To start the server
+
+<pre>yarn start</pre>
+
+### More
+
+Modify pages content in  `src/content_option.js`.
---
+++ src/content_option.js
@@ -0,0 +1,124 @@
+const logotext = ":3";
+const meta = {
+    title: "catala.dev",
+    description: "Aspiring Software Engineer",
+};
+
+const introdata = {
+    title: "Carlos Catala",
+    animated: {
+        first: "AI Engineer",
+        second: "Software Engineer",
+    },
+    description: "University of Central Florida Freshman",
+    your_img_url: "https://i1.sndcdn.com/artworks-zyYqA8D0BdfuyH28-WeeHrw-t500x500.jpg",
+};
+
+const dataabout = {
+    title: "About Myself",
+    aboutme: "For a long time, I struggled to find my passion. That changed when I built my first PC. I was instantly captivated by the intricate world of computer hardware. But curiosity gnawed at me: how did these components actually function? My search for answers led me to Python, igniting a lifelong exploration of programming.",
+};
+const worktimeline = [{
+        jobtitle: "Game Programmer",
+        where: "Crowning Games",
+        date: "June 2024 - Present\n\n",
+    },
+    {
+        jobtitle: "Junior Learning Engineer",
+        where: "Miami EdTech",
+        date: "June 2023 - July 2023\n\n",
+    },
+    {
+        jobtitle: "AI Tester",
+        where: "Avatar Buddy",
+        date: "June 2022 - July 2022\n\n",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "C#",
+        value: 75,
+    },
+    {
+        name: "CSS",
+        value: 75,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "HTML",
+        value: 60,
+    },
+    {
+        name: "Java",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design, turn this on if u want",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.postimg.cc/rFqFvj5w/tsa-cover.png",
+        description: "Night of Knights was my first ever large-scale project, it was done as a capstone project senior year of high school. When you play it, keep in mind its a bit buggy.",
+        link: "https://play.unity.com/en/games/27ee33fa-e1e3-4178-8acf-9e1f37546fd0/night-of-knights",
+    },
+    {
+        img: "https://i.postimg.cc/ydcMLqdY/mainmenu-1.png",
+        description: "This was my second large scale project, I am working on it with some friends, we ended up posting it on Steam! (Ignore bad quality of picture, still don't have a good one.)",
+        link: "https://store.steampowered.com/app/3074570/Keena_The_VoidSlayer/",
+    },
+    {
+        img: "https://tr.rbxcdn.com/61d4ba809099fcebadbe6c8dd6d67306/420/420/Hat/Webp",
+        description: "It's the website you're on right now!",
+        link: "https://www.catala.dev/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Add YOUR email here",
+    description: "You can also message me on LinkedIn!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "add your service id",
+    YOUR_TEMPLATE_ID: "add your template id",
+    YOUR_USER_ID: "add ur public user id",
+};
+
+const socialprofils = {
+    github: "https://github.com/cataladev",
+    linkedin: "https://www.linkedin.com/in/carlos-catala-516130299/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
